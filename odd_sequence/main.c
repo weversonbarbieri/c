fc52660e8d67835ff4c47867061ecee5030e28e0
weversonbarbieri <weverson.barbieri@gmail.com>
2024-02-18 13:20:08 -0400

    Odd sequence - C
    Enter an integer X value. Then the algorithm prints the odd numbers from 1 to X, one value per line, including X, if that's the case.

---
+++ odd_sequence/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x, i, odd;
+
+    printf("Enter X number: ");
+    scanf("%d", &x);
+
+    for (i = 0; i < x; i++) {
+        i = i + 1;
+        printf("%d\n", i);
+    }
+
+
+
+
+    return 0;
+}
